a23d76cab87d1f9d90fdd2d5562e8dfb178f361e
unknown <braynyuwono@gmail.com>
2025-12-01 05:31:33 +0700

    sedikit perbaikan auth

--- database/db.go
+++ database/db.go
@@ -25,21 +25,3 @@ func ConnectDB() *sql.DB {
 	log.Println("Successfully connected to database")
 	return db
 }
-// func ConnectDB() *sql.DB {
-//     dsn := os.Getenv("DB_DSN")
-//     if dsn == "" {
-//         log.Fatal("❌ DB_DSN environment variable is not set")
-//     }
-
-//     db, err := sql.Open("postgres", dsn)
-//     if err != nil {
-//         log.Fatalf("❌ Failed to open connection: %v", err)
-//     }
-
-//     if err := db.Ping(); err != nil {
-//         log.Fatalf("❌ Failed to connect to database: %v", err)
-//     }
-
-//     log.Println("✅ Connected to database.")
-//     return db
-// }
